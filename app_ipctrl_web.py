dde9c1a063804299b9ca6d63bf89da78e68936a3
anovikov <anovikovpost@gmail.com>
2023-07-18 14:44:42 +0300

    очередь:работает, айди девайса в логе, если девайс залогинен, три попытки экспешн и завершение.

--- app_ipctrl_web.py
+++ app_ipctrl_web.py
@@ -19,7 +19,7 @@ from urllib.parse import urlparse, parse_qs
 # os.environ['JAVA_HOME'] = "/usr/lib/jvm/java-16-openjdk-amd64"
 
 # Configure logging
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+#logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=self.logger.info)
 
 # Maximum attempts to locate the element
 max_attempts = 3
@@ -29,23 +29,23 @@ def battery_opt():
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     if stdout:
-        logging.info('Output: ', stdout.decode())
+        self.logger.info("Output: ", stdout.decode())
     if stderr:
-        logging.info('Error: ', stderr.decode())
+        self.logger.info("Error: ", stderr.decode())
 
-def open_iproxy(device_serial, device_id):
+def open_iproxy(device_serial, device_id, logger):
     # Run the ADB command to check the focused application
     adb_command = f'adb -s {device_serial} shell dumpsys window windows | grep -E "mCurrentFocus|mFocusedApp"'
     process = subprocess.Popen(adb_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
 
     if stderr:
-        logging.error('Error: %s', stderr.decode())
+        logger.error('Error: %s', stderr.decode())
         return
 
     # Check if the output contains the package name
     if b'com.iproxy.android' in stdout:
-        logging.info('The application is already on the screen.')
+        logger.info("The application is already on the screen.")
     else:
         # Application is in the background, start it
         start_command = f'adb -s {device_serial} shell am start -n com.iproxy.android/com.iproxy.android.MainActivity'
@@ -53,9 +53,9 @@ def open_iproxy(device_serial, device_id):
         start_stdout, start_stderr = start_process.communicate()
 
         if start_stdout:
-            logging.info('Output: %s', start_stdout.decode())
+            logger.info(f"{self.device_id}: Output: %s", start_stdout.decode())
         if start_stderr:
-            logging.error('Error: %s', start_stderr.decode())
+            logger.error('Error: %s', start_stderr.decode())
 
 # Create a queue to store the incoming requests
 request_queue = Queue()
@@ -68,7 +68,11 @@ class CustomRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(message.encode())
+        try:
+            self.wfile.write(message.encode())
+        except BrokenPipeError:
+            self.logger.info("Client closed the connection before the response could be sent.")
+            pass  # or log error message if you prefer
 
     def do_GET(self):
         # Parse URL
@@ -89,7 +93,7 @@ class CustomRequestHandler(BaseHTTPRequestHandler):
                 self._send_response('Login done')
 
             elif params.get('logout') == ['true']:
-                self.device_queues[device_serial].put(('dummy_pin', device_id, 'test_logout'))  # assuming pin is not required for logout
+                self.device_queues[device_serial].put(('None', device_id, 'test_logout'))  # assuming pin is not required for logout
                 self._send_response('Logout done')
 
             else:
@@ -106,8 +110,6 @@ class CustomRequestHandler(BaseHTTPRequestHandler):
             suite.addTest(TestAppiumWithPin(pin, device_serial, device_id, test_name))
             unittest.TextTestRunner().run(suite)
 
-#num_worker_threads = 2 # Количество рабочих потоков
-
 class TestAppium(unittest.TestCase):
     driver = None
     wait = None
@@ -121,12 +123,25 @@ class TestAppium(unittest.TestCase):
         if cls.driver is not None:
             cls.driver.quit()
 
-class TestAppiumWithPin(TestAppium):
+class AlreadyLoggedInException(Exception):
+        pass
+
+class TestAppiumWithPin(TestAppium):   
     def __init__(self, pin, device_serial, device_id, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.pin = pin
         self.device_serial = device_serial
         self.device_id = device_id
+
+        # Set up a logger for this device
+        self.logger = logging.getLogger(self.device_id)
+        self.logger.setLevel(logging.INFO)  # Set logging level to INFO
+
+        if not self.logger.handlers:  # Add handler only if there are no handlers added previously
+            handler = logging.StreamHandler()
+            formatter = logging.Formatter(f'%(asctime)s %(name)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
 
     def setUp(self): #set capabilities for each test separately
         capabilities = dict(
@@ -142,17 +157,21 @@ class TestAppiumWithPin(TestAppium):
     def tearDown(self):
         if self.driver is not None:
             self.driver.quit()
-    
+
     def test_login(self) -> None:
         max_attempts = 3
         PIN = self.pin  # Consider retrieving this from a more secure place
-        logging.info('test_login started')
-        open_iproxy(self.device_serial, self.device_id)
-        logging.info('iproxy opened')
+        self.logger.info('test_login started')
+        open_iproxy(self.device_serial, self.device_id, self.logger)
+        self.logger.info("iproxy opened")
 
         for attempt in range(max_attempts):
             try:
-                self.click_use_pin()
+                try:
+                    self.click_use_pin()
+                except NoSuchElementException:
+                    self.check_status()
+                    break
                 self.input_pin(PIN)
                 self.click_login()
                 self.selecting_connection()
@@ -162,31 +181,33 @@ class TestAppiumWithPin(TestAppium):
                 # Проверка успешного входа в систему
                 try:
                     WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//android.widget.TextView[@text="Proxy"]')))
-                    logging.info("Successfully LOGGED IN")
+                    self.logger.info("Successfully LOGGED IN")
 
                     #check proxy status
                     proxy_status = self.toggle_status()
                     if proxy_status == "Proxy is disabled":
                         self.proxy_switcher()  # call proxy_switcher if proxy is disabled
                     elif proxy_status == "Proxy is enabled":
-                        logging.info("Proxy is already enabled")
+                        self.logger.info("Proxy is already enabled")
 
                     break
 
                 except TimeoutException:
-                    logging.info("LOGIN unsuccessful. Retrying...")
+                    self.logger.info("LOGIN unsuccessful. Retrying...")
                     continue
 
-
             except (StaleElementReferenceException, NoSuchElementException, TimeoutException) as e:
-                logging.info(f"Error during login attempt {attempt + 1}: {e}. Retrying...")
+                self.logger.info(f"Error during login attempt {attempt + 1}: {e}. Retrying...")
                 continue
+
+
+
 
     def test_logout(self):
         max_attempts = 3
-        logging.info('test_logout started')
-        open_iproxy(self.device_serial, self.device_id)
-        logging.info('iproxy opened')
+        self.logger.info("test_logout started")
+        open_iproxy(self.device_serial, self.device_id, self.logger)
+        self.logger.info("iproxy opened")
 
         for attempt in range(max_attempts):
             try:
@@ -198,140 +219,154 @@ class TestAppiumWithPin(TestAppium):
                 # Проверка успешного входа в систему
                 try:
                     logout_status = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//android.widget.TextView[@text="LOG IN"]')))
-                    logging.info("Successfully LOGGED OUT")
+                    self.logger.info("Successfully LOGGED OUT")
                     return logout_status.text
 
                 except TimeoutException:
-                    logging.info("Logout unsuccessful. Retrying...")
+                    self.logger.info("Logout unsuccessful. Retrying...")
                     continue
 
             except (StaleElementReferenceException, NoSuchElementException, TimeoutException) as e:
-                logging.info(f"Error during logout attempt {attempt + 1}: {e}. Retrying...")
+                self.logger.info(f"Error during logout attempt {attempt + 1}: {e}. Retrying...")
                 continue
 
     def click_use_pin(self) -> None:
-        logging.info("starting function 'click_use_pin'")
+        self.logger.info("starting function 'click_use_pin'")
         usepin_button = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//android.widget.TextView[@text="USE PIN"]')))
-        logging.info("found button 'USE PIN'")
+        self.logger.info("found button 'USE PIN'")
         usepin_button.click()
-        logging.info("clicked 'USE PIN'")
+        self.logger.info("clicked 'USE PIN'")
 
     def input_pin(self, PIN: str) -> None:
-        logging.info("starting function 'input_pin'")
+        self.logger.info("starting function 'input_pin'")
         pin_field = self.driver.find_element(By.XPATH, '//android.widget.ScrollView[@index="0"]/android.widget.EditText[@index="1"]')
-        logging.info("found input 'Connection PIN'")
+        self.logger.info("found input 'Connection PIN'")
         pin_field.clear()
-        logging.info("input field was cleared")
+        self.logger.info("input field was cleared")
         pin_field.send_keys(PIN)
-        logging.info(f'Input {PIN}')
+        self.logger.info(f'Input {PIN}')
 
     def click_login(self) -> None:
-        logging.info("starting function 'click_login'")
+        self.logger.info("starting function 'click_login'")
         click_login = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//android.widget.TextView[@text="LOG IN"]')))
-        logging.info("found button 'LOG IN'")
+        self.logger.info("found button 'LOG IN'")
         click_login.click()
-        logging.info("clicked 'LOG IN'")
+        self.logger.info("clicked 'LOG IN'")
 
     def popup_in_use(self):
         try:
             # Ожидаем появления всплывающего окна в течение 5 секунд
-            logging.info("starting function 'popup_in_use'")
+            self.logger.info("starting function 'popup_in_use'")
             popup_continue = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//android.widget.TextView[@text="Continue"]')))
-            logging.info("found button 'continue'")
+            self.logger.info("found button 'continue'")
             popup_continue.click()
-            logging.info("Clicked continue")
+            self.logger.info("Clicked continue")
         except TimeoutException:
             # Если всплывающее окно не появилось, продолжаем выполнение кода
-            logging.info("Popup connection is in use NOT APPEARED")
+            self.logger.info("Popup connection is in use NOT APPEARED")
     
     def selecting_connection(self):
         try:
             # Ожидаем появления всплывающего окна в течение 10 секунд
-            logging.info("starting function 'selecting_connection'")
+            self.logger.info("starting function 'selecting_connection'")
             popup_selecting = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//android.widget.TextView[@text="Selecting connection…"]')))
-            logging.info(f"found popup with text {popup_selecting.text}")
+            self.logger.info(f"found popup with text {popup_selecting.text}")
 
             # Ожидаем исчезновения всплывающего окна в течение 30 секунд
             WebDriverWait(self.driver, 30).until(EC.invisibility_of_element_located((By.XPATH, '//android.widget.TextView[@text="Selecting connection…"]')))
-            logging.info("popup 'selecting_connection' DISSAPEARED")
+            self.logger.info("popup 'selecting_connection' DISSAPEARED")
 
         except TimeoutException:
             # Если всплывающее окно не появилось, продолжаем выполнение кода
-            logging.info("Popup 'selecting_connection' NOT APPEARED")
+            self.logger.info("Popup 'selecting_connection' NOT APPEARED")
 
     def toggle_status(self):
-        logging.info("starting function 'toggle_status'")
+        self.logger.info("starting function 'toggle_status'")
         try:
             toggle_status = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//android.widget.ScrollView[@index="0"]/android.view.View[@index="0"]/android.view.View[@index="5"]/android.widget.TextView[@index="1"]')))
-            logging.info(toggle_status.text)
+            self.logger.info(toggle_status.text)
             return toggle_status.text
         except TimeoutException:
-            logging.info("Toggle status element not found. Continuing without it...")
+            self.logger.info("Toggle status element not found. Continuing without it...")
             return None
 
     def proxy_switcher(self):
-        logging.info("starting function 'proxy_switcher'")
+        self.logger.info("starting function 'proxy_switcher'")
         try:
             proxy_switcher = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//android.widget.ScrollView[@index="0"]/android.view.View[@index="0"]/android.view.View[@index="5"]')))
-            logging.info("found element proxy_switcher")
+            self.logger.info("found element proxy_switcher")
             proxy_switcher.click()
-            logging.info("proxy switched ON")
+            self.logger.info("proxy switched ON")
         except TimeoutException:
-            logging.info("Toggle status element not found. Continuing without it...")
+            self.logger.info("Toggle status element not found. Continuing without it...")
 
     def click_more(self):
-        logging.info("starting function 'click_more'")
+        self.logger.info("starting function 'click_more'")
         try:
             click_more = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//android.view.View[@content-desc="Options"]')))
-            logging.info("found element MORE")
+            self.logger.info("found element MORE")
             click_more.click()
-            logging.info("tap MORE")
+            self.logger.info("tap MORE")
         except TimeoutException:
-            logging.info("element MORE not found. Continuing without it...")
+            self.logger.info("element MORE not found. Continuing without it...")
 
     def chose_logout(self):
-        logging.info("starting function 'chose_logout'")
+        self.logger.info("starting function 'chose_logout'")
         try:
             chose_logout = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//android.widget.ScrollView[@index="0"]/android.view.View[@index="6"]/android.widget.TextView[@index="0"]')))
-            logging.info("found element LOGOUT")
+            self.logger.info("found element LOGOUT")
             chose_logout.click()
-            logging.info("tap LOGOUT")
+            self.logger.info("tap LOGOUT")
         except TimeoutException:
-            logging.info("element LOGOUT not found. Continuing without it...")
+            self.logger.info("element LOGOUT not found. Continuing without it...")
 
     def confirm_logout(self):
-        logging.info("starting function 'confirm_logout'")
+        self.logger.info("starting function 'confirm_logout'")
         try:
             confirm_logout = self.wait.until(EC.visibility_of_element_located((By.ID, 'android:id/button1')))
-            logging.info("found element confirm_LOGOUT")
+            self.logger.info("found element confirm_LOGOUT")
             confirm_logout.click()
-            logging.info("tap confirm_LOGOUT")
+            self.logger.info("tap confirm_LOGOUT")
         except TimeoutException:
-            logging.info("element confirm_LOGOUT not found. Continuing without it...")
+            self.logger.info("element confirm_LOGOUT not found. Continuing without it...")
 
     def signing_out(self):
         try:
             # Ожидаем появления элемента
-            logging.info("starting function 'signing_out'")
+            self.logger.info("starting function 'signing_out'")
             popup_signing_out = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//android.widget.TextView[@text="Signing out…"]')))
             #print(popup_signing_out.text)
-            logging.info(f"found popup with text {popup_signing_out.text}")
+            self.logger.info(f"found popup with text {popup_signing_out.text}")
 
             # Ожидаем, когда элемент станет устаревшим
             WebDriverWait(self.driver, 30).until(EC.staleness_of(popup_signing_out))
-            logging.info("popup 'signing_out' DISAPPEARED")
+            self.logger.info("popup 'signing_out' DISAPPEARED")
 
         except TimeoutException:
             # Если элемент не появился или не исчез в заданный период времени
-            logging.info("Popup 'signing_out' NOT APPEARED or DID NOT DISAPPEAR in time")
+            self.logger.info("Popup 'signing_out' NOT APPEARED or DID NOT DISAPPEAR in time")
 
     def scroll_to_text(driver, text):
         element = driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
                                     'new UiScrollable(new UiSelector().resourceId("android:id/list")).scrollIntoView(new UiSelector().text("'+ text +'"))')
         return element
 
+    def check_status(self) -> None:
+        self.logger.info("starting function 'check_status'")
+        try:
+            status_text = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//android.widget.TextView[@text="Status"]')))
+            self.logger.info("found text 'status'")
+            
+            if status_text:
+                #self.logger.info("You are already logged in.")
+                raise AlreadyLoggedInException("You are already logged in.")
+            else:
+                status_button.click()
+                self.logger.info("clicked 'status'")
+        except NoSuchElementException:
+            self.logger.info("Status element not found. Continuing without it...")
+
 if __name__ == '__main__':
     server_address = ('', 8000)
     httpd = HTTPServer(server_address, CustomRequestHandler)
-    logging.info('Starting server...')
     httpd.serve_forever()
